4f44af6ed2ce8b5f335aa6991d161a2aaaca53ac
Prue0115 <prue0115@gmail.com>
2025-05-09 15:58:02 +0900

    개인 정보 출력 프로그램

---
+++ Practice_4_1/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        String name = "홍길동";
+        int age = 20;
+        System.out.println(name + "님은 " + age + "살 입니다.");
+    }
+}
